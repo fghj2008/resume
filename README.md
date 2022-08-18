34df0570bdf51e1a524d0124492c94dd560998dd
fghj2008 <31117468+fghj2008@users.noreply.github.com>
2022-08-18 09:51:45 +0800

    all

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # resume
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/wert2008/pen/yZVPdv](https://codepen.io/wert2008/pen/yZVPdv).
+
---
+++ script.js
@@ -0,0 +1,18 @@
+$('.slick').slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  prevArrow: false,
+  nextArrow: false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+$('.poster').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  prevArrow: false,
+  nextArrow: false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
